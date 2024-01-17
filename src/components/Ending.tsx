866f4880358f1cd9a1aaa158a3fe0d7eba0201ab
JunDemi <jungwook3176@gmail.com>
2024-01-17 14:27:11 +0900

    Fix: 문의하기 오픈 카카오톡 링크

--- src/components/Ending.tsx
+++ src/components/Ending.tsx
@@ -1,5 +1,6 @@
 import { motion, useInView } from "framer-motion";
 import React, { useRef } from "react";
+import { Link } from "react-router-dom";
 
 function Ending() {
   const ending = useRef(null);
@@ -13,7 +14,9 @@ function Ending() {
         transition={{duration: 0.8, delay: 0.3}}
         className="lg:text-4xl text-xl text-center">저희 두빛나래 길드와 함께<br/> 성장해 나가실 길드원을 모집합니다!</motion.h1>
         <img src="https://res.cloudinary.com/dgmgeotyk/image/upload/v1697623233/competition-uploads/vnqzfrwlegxadvmpu3uf.jpg" alt="" className="lg:w-24 w-16 rounded-lg"/>
-        <span className="bg-gray-800 text-white px-10 py-4 rounded-full hover:bg-gray-600 transition lg:text-base text-sm">문의하기</span>
+        <Link to="https://open.kakao.com/o/gwtzR8N" target="_blank">
+          <span className="bg-gray-800 text-white px-10 py-4 rounded-full hover:bg-gray-600 transition lg:text-base text-sm">문의하기</span>
+        </Link>
       </div>
     </>
   );
